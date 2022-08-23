06cbf1739e79350fd7552ad2f5d8b56f3426148f
ddobrynina <ddobrynina924@gmail.com>
2022-08-23 16:52:30 -0400

    Add the etch-a-sketch grid and way to change size of the grid

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
-const container = document.querySelector("container");
-
-function makeGrid(rows, cols) {
-  for (i = 0; i < (rows * cols); i++) {
-    let cell = document.createElement("div");
-    cell.classList.add('grid-item');
-    container.appendChild(cell);
+const container = document.querySelector("#container");
+function makeGrid(nrows, ncols) {
+  for (let i = 0; i < nrows; i++) {
+    let row = document.createElement("div");
+    row.classList.add('row');
+    container.appendChild(row);
+    for (let j=0; j < ncols; j++)
+    {
+      let col = document.createElement("div");
+      col.classList.add('col');
+      row.appendChild(col);
+    }
   };
 };
-makeGrid(16,16);
+
+makeGrid(rows, cols);
+
+const selectBtn = document.querySelector("#select");
+selectBtn.addEventListener("click", changeGridSize);
+
+
+container.addEventListener("mouseover", highlight, true);
+container.addEventListener("mouseout", normalColor, true);
+container.addEventListener("click", changeColor, true);
+
+function changeGridSize()
+{
+  let newValue = document.querySelector("input").value;
+  newValue = parseInt(newValue);
+    if(newValue > 1 && 101 > newValue)
+    {
+      console.log(newValue);
+        while(container.firstChild)
+        {
+          container.removeChild(container.firstChild);
+        }
+        makeGrid(newValue, newValue);
+    }
+}
+
+function highlight(e)
+{
+  if (e.srcElement.style.backgroundColor != "black")
+  {
+    e.srcElement.style.backgroundColor = "blue";
+  }
+}
+
+function normalColor(e)
+{
+  if (e.srcElement.style.backgroundColor != "black")
+  {
+    e.srcElement.style.backgroundColor = "white";
+  }
+}
+
+function changeColor(e)
+{
+  e.srcElement.style.backgroundColor = "black";
+}
+
